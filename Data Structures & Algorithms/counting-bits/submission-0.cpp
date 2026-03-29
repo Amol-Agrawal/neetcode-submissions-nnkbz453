3334033179a164fc7f8e42164a023f74ba401cc4
Amol-Agrawal <amolagrawal2025@gmail.com>
2026-03-30 04:06:48 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1,0);
+        for(int i = 0; i<=n ; i++){
+            int k = i;
+            while(k!=0 ){
+                ans[i] += k%2;
+                k= k/2;
+            }
+
+        }
+        return ans;
+    }
+};
