99b156ef01f69033c5a69f8cef854b5781e7b89f
Amol-Agrawal <amolagrawal2025@gmail.com>
2026-04-11 13:19:32 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.cpp
@@ -0,0 +1,44 @@
+class MedianFinder {
+public:
+
+    priority_queue<int> small ;
+    priority_queue<int, vector<int>, greater<int>> large;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        small.push(num);
+
+        if(!large.empty() && small.top()> large.top()){
+            large.push(small.top());
+            small.pop();
+        }
+
+        if(small.size() > large.size() +1){
+            large.push(small.top());
+            small.pop();
+        }
+
+        if( large.size() > small.size() +1 ){
+            small.push(large.top());
+            large.pop();
+        }
+
+        
+    }
+    
+    double findMedian() {
+
+        if(large.size() == small.size()){
+            return (double)(large.top() + small.top())/2;
+        }
+
+        if(small.size() > large.size()){
+            return small.top();
+        }else{
+            return large.top();
+        }
+        
+    }
+};
